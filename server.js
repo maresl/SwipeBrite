e94d50f4e39461a2b01845336ce8ce8eef93c42f
Armando Xhimanki <armando.xhimanki@gmail.com>
2021-03-15 21:08:19 -0400

    forEach is not asynchronous. You cannot do await inside of it.

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const cors = require('cors')
-const indexRouter = require('./routes/index')
-require('dotenv').config()
-
+const express = require("express");
+const cors = require("cors");
+const indexRouter = require("./routes/index");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
-require('./config/database')
-
+require("./config/database");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
-app.use( cors() )
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use( ( req, res, next ) => {
-    console.log(`${req.method} ${req.originalUrl}`)
-    next();
-  });
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
 
-app.use('/api/v1/events', indexRouter.events);
-app.use('/api/v1/auth', indexRouter.auth);
+app.use("/api/v1/events", indexRouter.events);
+app.use("/api/v1/auth", indexRouter.auth);
 
-app.listen( PORT, () => console.log( `listing at port ${PORT} \nhttp://localhost:${PORT}/api/v1/events`) );
+app.listen(PORT, () =>
+  console.log(
+    `listing at port ${PORT} \nhttp://localhost:${PORT}/api/v1/events`
+  )
+);
