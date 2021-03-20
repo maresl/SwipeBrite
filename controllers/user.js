fb7abf2cc3499b6bd939ece83c3bdb2dbf927495
Sean Yates <seanyates@Seans-MacBook-Pro.local>
2021-03-20 16:23:20 -0700

    event removed from queue after voting

--- controllers/user.js
+++ controllers/user.js
@@ -9,45 +9,52 @@ Expecting: req to contain whether they liked, disliked, or blacklisted the event
 i.e. req.body.decision = blacklist, req.body.decision = liked, req.body.decision = disliked,  
 */
 const updateUserEventPreferences = async (req, res) => {
-  console.log('got into updateUserEventPreferences')
-  try {
-    console.log('got into try catch')
-    const foundUser = await User.findById(req.user.id);
 
-    console.log('id of the user', foundUser._id)
+  try {
+
+    const foundUser = await User.findById(req.user.id);
 
     let existingEvent = await Event.findOne({
       eventID: req.body.eventID,
     })
 
-    console.log('id of the event', existingEvent._id)
-    //added this util fcn just in case what we get does not match what we expect.
-
-    //$addToSet works on arrays, and if the user decision didnt exist in our db it would create it.
     const filteredDecision = filter(req.body.decision);
+    //console.log("eventQueue:", foundUser.eventQueue)
 
     if (filteredDecision) {
-      console.log(`gonna add ${existingEvent._id} to the ${filteredDecision} array now`)
+  
+      foundUser[filteredDecision].push(existingEvent._id)
 
-      await foundUser[filteredDecision].push(existingEvent._id)
+      console.log("foundUser.eventQueue.indexOf(existingEvent._id)", foundUser.eventQueue.indexOf(existingEvent._id))
+      foundUser.eventQueue.splice(foundUser.eventQueue.indexOf(existingEvent._id),1)
+
       await foundUser.save()
+    } else {
+      throw "invalidDecision"
     }
-      console.log("liked events:", foundUser.likedEvents)
-      console.log("disliked events:", foundUser.dislikedEvents)
-      console.log("blacklist events:", foundUser.blacklistEvents)
-
-
+      // console.log("liked events:", foundUser.likedEvents)
+      // console.log("disliked events:", foundUser.dislikedEvents)
+      // console.log("blacklist events:", foundUser.blacklistEvents)
+      //console.log("eventQueue:", foundUser.eventQueue)
 
     res.status(200).json({
       status: 200,
       requestedAt: new Date().toLocaleString(),
     });
   } catch (err) {
-    res.status(500).json({
-      status: 500,
-      message: err,
-      requestAt: new Date().toLocaleString(),
-    });
+    if (err === "invalidDecision") {
+      res.status(422).json({
+        status: 422,
+        message: "Unprocessable entity, invalid decision on event",
+        requestAt: new Date().toLocaleString(),
+      });
+    } else {
+      res.status(500).json({
+        status: 500,
+        message: err,
+        requestAt: new Date().toLocaleString(),
+      });
+    }
   }
 };
 
